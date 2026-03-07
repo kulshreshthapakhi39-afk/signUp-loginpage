46527e0a4f3b962ba5544dd6a88e2f5f5f01e8e6
kulshreshthapakhi39-afk <kulshreshthapakhi39@gmail.com>
2026-03-07 20:00:13 +0530

    create new page

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSignup(){
+    document.getElementById("home").style.display="none";
+    document.getElementById("login").style.display="none";
+    document.getElementById("signup").style.display="flex";
+}
+
+function showLogin(){
+    document.getElementById("home").style.display="none";
+    document.getElementById("signup").style.display="none";
+    document.getElementById("login").style.display="flex"
+}
