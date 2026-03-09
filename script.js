455fbed0ba74c0ea0c9805b6e1f8a11da522ce1e
kulshreshthapakhi39-afk <kulshreshthapakhi39@gmail.com>
2026-03-09 21:48:45 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function showSignup(){
-    document.getElementById("home").style.display="none";
-    document.getElementById("login").style.display="none";
-    document.getElementById("signup").style.display="flex";
+function showSignup() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("signup").style.display = "flex";
 }
 
-
-function showLogin(){
-    document.getElementById("home").style.display="none";
-    document.getElementById("signup").style.display="none";
-    document.getElementById("login").style.display="flex"
+function showLogin() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("login").style.display = "flex"
 }
